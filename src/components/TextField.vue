9a98a8dd80a05bd4e57d8fe85a2db2b9c833a74b
Pedro Manuel Martinez Pelayo <pedromanuel.martinez@viewnext.com>
2024-10-18 09:13:04 +0200

    La pagina de editar productos crea los nuevos productos correctamente

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,14 +1,14 @@
 <script setup>
-defineProps(['name', 'label', 'type'])
+defineProps(['name', 'label', 'type', 'required'])
 
 
 </script>
 
 <template>
 
-    <label class="px-3 py-2 flex flex-row gap-4 rounded-xl items-center">
-        <span class="font-bold">{{ label }}</span>
+    <label class="px-3 py-2 flex flex-row gap-4 rounded-xl items-center justify-between">
+        <span class="font-bold w-24">{{ label }}</span>
         <input class="block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" :type="type" :name="name">
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" :required="required" :type="type" :name="name">
     </label>
 </template>
--- src/pages/EditProductPage.vue
+++ src/pages/EditProductPage.vue
@@ -1,19 +1,61 @@
 <script setup>
+import { onMounted, ref, watchEffect } from 'vue';
 import TextField from '../components/TextField.vue';
+import axios from 'axios';
 
 defineProps(['productId'])
 
+const product = ref(null)
+const saveSuccess = ref(false)
+
+watchEffect(() => {
+    if(saveSuccess.value){
+        
+    }
+})
+
+function handleSubmit(e){
+    e.preventDefault()
+    
+    const formData = new FormData(e.target)
+    const entries = Object.fromEntries(formData.entries())
+    console.log(entries);
+
+    if(!product.value){
+        //crear
+        try{
+            axios.post(`${import.meta.env.VITE_API_URL}/products`, {
+                name: entries.name,
+                price : Number(entries.price),
+                category: entries.category,
+                stock: Number(entries.stock)
+            })
+            .then(() => {
+                saveSuccess.value = true
+                alert('Producto guardado correctamente')
+            })
+            
+        } catch(e){
+            alert('Error al guardar el producto', e.message)
+        }
+    }
+        
+}
 
 </script>
 
 <template>
-    <section class="w-full h-full flex flex-col items-center gap-2">
+    <section @submit="handleSubmit" class="w-full h-full flex flex-col items-center gap-2 px-4">
         <h1 class="mt-6 text-2xl font-bold uppercase">Edita un producto</h1>
-        <form class="mx-auto w-full flex flex-col gap-2 p-4 max-w-5xl border-2 bg-orange-300 rounded-2xl overflow-hidden">
-            <TextField type="text" label="Prueba:" name="name"></TextField>
-            <TextField type="number" label="Prueba:" name="name"></TextField>
-            <TextField type="text" label="Prueba:" name="name"></TextField>
-            <TextField type="text" label="Prueba:" name="name"></TextField>
+        <form class="mx-auto w-full flex flex-col gap-2 p-4 max-w-5xl border-2 bg-orange-300 rounded-2xl overflow-hidden shadow-lg">
+            <TextField type="text" label="Nombre:" name="name" required></TextField>
+            <TextField type="number" label="Precio:" name="price" required></TextField>
+            <TextField type="text" label="Categoria:" name="category" required></TextField>
+            <TextField type="number" label="Stock:" name="stock" required></TextField>
+
+            <footer class="flex justify-end">
+                <button class="shadow-md bg-green-500 px-4 py-1 rounded-xl border border-black text-lg transition-colors duration-300 hover:bg-green-600" >Guardar</button>
+            </footer>
         </form>
     </section>
 </template>
