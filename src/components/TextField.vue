049612d30aed27e5d0c929423ff8bceaccdf006d
Pedro Manuel Martinez Pelayo <pedromanuel.martinez@viewnext.com>
2024-10-18 12:18:30 +0200

    Agregada mas seguridad para la validacion de campos en el cliente

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,5 +1,5 @@
 <script setup>
-defineProps(['name', 'label', 'type', 'required', 'defaultValue'])
+defineProps(['name', 'label', 'type', 'required', 'defaultValue', 'min'])
 
 
 </script>
@@ -9,6 +9,7 @@ defineProps(['name', 'label', 'type', 'required', 'defaultValue'])
     <label class="px-3 py-2 flex flex-row gap-4 rounded-xl items-center justify-between">
         <span class="font-bold w-24">{{ label }}</span>
         <input class="block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" :value="defaultValue" :required="required" :type="type" :name="name">
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" 
+      :value="defaultValue" :required="required" :type="type" :name="name" step="any" :minLength="min">
     </label>
 </template>
