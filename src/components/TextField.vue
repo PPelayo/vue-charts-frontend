b4571650e29977d073412571078866b1f6643839
Pedro Manuel Martinez Pelayo <pedromanuel.martinez@viewnext.com>
2024-10-18 11:28:43 +0200

    La pagina para editar o crear productos recupera un producto si lo tiene que editar y ahora guarda los cambios correctamente

--- src/components/TextField.vue
+++ src/components/TextField.vue
@@ -1,5 +1,5 @@
 <script setup>
-defineProps(['name', 'label', 'type', 'required'])
+defineProps(['name', 'label', 'type', 'required', 'defaultValue'])
 
 
 </script>
@@ -9,6 +9,6 @@ defineProps(['name', 'label', 'type', 'required'])
     <label class="px-3 py-2 flex flex-row gap-4 rounded-xl items-center justify-between">
         <span class="font-bold w-24">{{ label }}</span>
         <input class="block w-full px-3 py-2 bg-white border border-gray-300 rounded-md text-sm shadow-sm
-      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" :required="required" :type="type" :name="name">
+      focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500" :value="defaultValue" :required="required" :type="type" :name="name">
     </label>
 </template>
--- src/pages/EditProductPage.vue
+++ src/pages/EditProductPage.vue
@@ -2,59 +2,106 @@
 import { onMounted, ref, watchEffect } from 'vue';
 import TextField from '../components/TextField.vue';
 import axios from 'axios';
+import { useRouter } from 'vue-router';
 
 defineProps(['productId'])
 
 const product = ref(null)
-const saveSuccess = ref(false)
+const router = useRouter()
+const loading = ref(false)
 
-watchEffect(() => {
-    if(saveSuccess.value){
+onMounted(() => {
+    const urlParams = new URLSearchParams(window.location.search)
+    if(urlParams.has('productId')){
+        
+        const productId = urlParams.get('productId')
+        loading.value = true
+
+        try{
+            axios.get(`${import.meta.env.VITE_API_URL}/products/${productId}`)
+            .then(result => {
+                product.value = result.data
+            })
+        } catch(e) {
+            console.log(e)
+        } finally{
+            loading.value = false
+        }
         
     }
 })
 
-function handleSubmit(e){
+const redirect = () => {
+    router.push({ path: '/products' })
+}
+
+const normalizeProduct = ({ name, price, category, stock }) => {
+    return {
+        name,
+        price: Number(price),
+        category,
+        stock: Number(stock)
+    }
+}
+
+const saveProduct = ({ normalizedProduct }) => {
+    try {
+        axios.post(`${import.meta.env.VITE_API_URL}/products`, normalizedProduct)
+            .then(() => {
+                redirect()
+            })
+
+    } catch (e) {
+        alert('Error al guardar el producto', e.message)
+    }
+}
+
+const updateProduct = ({ normalizedProduct }) => {
+    const productToUpdate = {
+        ...normalizedProduct,
+        id: product.value.id
+    }
+    try {
+        axios.put(`${import.meta.env.VITE_API_URL}/products/${product.value.id}`, productToUpdate)
+            .then(() => {
+                redirect()
+            })
+    } catch (e) {
+        alert('Error al guardar el producto', e.message)
+    }
+}
+
+const handleSubmit = (e) => {
     e.preventDefault()
-    
+
     const formData = new FormData(e.target)
     const entries = Object.fromEntries(formData.entries())
-    console.log(entries);
 
-    if(!product.value){
-        //crear
-        try{
-            axios.post(`${import.meta.env.VITE_API_URL}/products`, {
-                name: entries.name,
-                price : Number(entries.price),
-                category: entries.category,
-                stock: Number(entries.stock)
-            })
-            .then(() => {
-                saveSuccess.value = true
-                alert('Producto guardado correctamente')
-            })
-            
-        } catch(e){
-            alert('Error al guardar el producto', e.message)
-        }
-    }
-        
+    const normalizedProduct = normalizeProduct({ name: entries.name, price: entries.price, category: entries.category, stock: entries.stock })
+    if (!product.value) 
+        saveProduct({ normalizedProduct })
+
+    else 
+        updateProduct({ normalizedProduct })
+    
 }
 
 </script>
 
 <template>
-    <section @submit="handleSubmit" class="w-full h-full flex flex-col items-center gap-2 px-4">
+    <h3 v-if="loading" class="w-full h-full flex items-center justify-center">Cargando...</h3>
+    <section v-else @submit="handleSubmit" class="w-full h-full flex flex-col items-center gap-2 px-4">
         <h1 class="mt-6 text-2xl font-bold uppercase">Edita un producto</h1>
-        <form class="mx-auto w-full flex flex-col gap-2 p-4 max-w-5xl border-2 bg-orange-300 rounded-2xl overflow-hidden shadow-lg">
-            <TextField type="text" label="Nombre:" name="name" required></TextField>
-            <TextField type="number" label="Precio:" name="price" required></TextField>
-            <TextField type="text" label="Categoria:" name="category" required></TextField>
-            <TextField type="number" label="Stock:" name="stock" required></TextField>
+        <form
+            class="mx-auto w-full flex flex-col gap-2 p-4 max-w-5xl border-2 bg-orange-300 rounded-2xl overflow-hidden shadow-lg">
+            <TextField type="text" label="Nombre:" name="name" required :defaultValue="product ? product.name : ''"></TextField>
+            <TextField type="number" label="Precio:" name="price" required :defaultValue="product ? product.price : 0"></TextField>
+            <TextField type="text" label="Categoria:" name="category" required :defaultValue="product ? product.category : ''"></TextField>
+            <TextField type="number" label="Stock:" name="stock" required :defaultValue="product ? product.stock : 0"></TextField>
 
-            <footer class="flex justify-end">
-                <button class="shadow-md bg-green-500 px-4 py-1 rounded-xl border border-black text-lg transition-colors duration-300 hover:bg-green-600" >Guardar</button>
+            <footer class="flex justify-end px-3">
+                <button
+                    class="shadow-md bg-green-500 px-4 py-1 rounded-xl border border-black text-lg transition-colors duration-300 hover:bg-green-600">Guardar</button>
             </footer>
         </form>
     </section>
